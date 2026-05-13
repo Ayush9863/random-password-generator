1b738271a811464a6ab7c60bc6209eeb40eb22c2
Ayush <ayushrao299@gmail.com>
2026-05-13 17:58:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ let symbolEl=document.getElementById("symbols")
 function run(){
      pass_1El.textContent=""
      pass_2El.textContent=""
-     let size=Number(lengthEl.value)//.value==>input field ke liye use hota hai==>Jo user type karta hai usko lene ke liye use hota hai
-     if(size<0 || size===0){//Number()==>string ko number mein convert karne ke liye use hota hai
+     let size=Number(lengthEl.value)
+     if(size<0 || size===0){
      pass_1El.textContent="Please Enter Suitable Length"
      pass_2El.textContent=""
-     return//mtlab function yahi band code aage nhi chalega
+     return
      }
      let array=[]
      if(uppercaseEl.checked){
@@ -54,20 +54,13 @@ function run(){
     for(let i=0;i<size;i++){
         let pass=Math.floor(Math.random()*array.length)
         let passes=Math.floor(Math.random()*array.length)
-// Math.random() ==> Yeh 0 se leke 0.999... ke beech ka koi bhi random decimal number deta hai (jaise 0.57, 0.12, etc.)
-// * array.length ==> Jab hum random number ko array ki total length se multiply karte hain, toh hume 0 se array ki aakhri index tak ka number milta hai (jaise 0.57 * 20 = 11.4)
-// Math.floor() ==> Yeh function kisi bhi decimal number ko round down karke ek seedha integer (whole number) bana deta hai (jaise 11.4 ban jayega 11, aur 11.9 bhi ban jayega 11).
-// Aisa isliye karte hain kyunki array ka index hamesha whole number hota hai (0, 1, 2, 3...), decimals mein nahi hota.
         pass_1El.textContent+=array[pass]
         pass_2El.textContent+=array[passes]
     }
 }
 function copy_1(){
     navigator.clipboard.writeText(pass_1El.textContent)
-    // .textContent → password le raha hai
-    // clipboard.writeText() → copy kar raha hai
-    alert("Copied!")//alert() = browser ka popup message
-    //jab password copy ho jayega==>ek popup message aayega "Copied!"
+    alert("Copied!")
 }
 
 function copy_2(){
